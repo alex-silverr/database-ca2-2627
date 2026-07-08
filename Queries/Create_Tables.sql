0874e2134c48cf822d0bb04a645971eb1bc336ce
Alex <alexpersiv@gmail.com>
2026-07-08 12:02:11 +0100

    cont

--- Queries/Create_Tables.sql
+++ Queries/Create_Tables.sql
@@ -1,8 +1,19 @@
-CREATE TABLE Languages()
+CREATE TABLE Languages(
+    ID INT(250) AUTO_INCREMENT PRIMARY KEY,
+    -- BookLanguage renamed from Language due to reserved keyword
+    BookLanguage VARCHAR(25) NOT NULL UNIQUE
+);
 
-CREATE TABLE BookGenres()
+CREATE TABLE BookGenres(
+    ID INT(50) AUTO_INCREMENT PRIMARY KEY,
+    Genre VARCHAR(15) NOT NULL UNIQUE
+);
 
-CREATE TABLE AvailabilityStatuses()
+CREATE TABLE AvailabilityStatuses(
+    ID INT(4) AUTO_INCREMENT PRIMARY KEY,
+    -- AvailStatus renamed from Status due to reserved keyword
+    AvailStatus VARCHAR(25) NOT NULL UNIQUE 
+);
 
 CREATE TABLE RentalStatuses()
 
@@ -16,8 +27,16 @@ CREATE TABLE Authors()
 
 CREATE TABLE Publishers()
 
+CREATE TABLE Members()
+
+CREATE TABLE Credentials()
+
+CREATE TABLE Staff()
+
 CREATE TABLE Books (
     BookBarcode VARCHAR(40) PRIMARY KEY,
     Title VARCHAR(200) NOT NULL,
     Publisher 
 )
+
+CREATE TABLE RentRecords()
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Deliverables:
 - Zip folder containing all the SQL files
 - Report (format up to choice, can be .pdf)
+
+# Renames
+- Language -> BookLanguage (Languages table)
+- Status -> AvailStatus (AvailabilityStatuses table)
