b1287ef0bfcf5c237b9b66991fde681e937ff3d3
Alex <alexpersiv@gmail.com>
2026-07-17 21:09:54 +0100

    populate book genres

---
+++ Queries/Populate_BookGenres.sql
@@ -0,0 +1,14 @@
+USE Library;
+
+INSERT INTO BookGenres (Genre)
+VALUES
+('Science'),
+('Childrens lit.'),
+('Young Adult'),
+('Horror'),
+('Philosophy'),
+('Classics'),
+('Fantasy'),
+('Poetry'),
+('Science Fiction'),
+('Biographies')
